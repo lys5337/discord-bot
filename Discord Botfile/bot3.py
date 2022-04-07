6038e211c0336ea2c5a6833ae3b3ed0efc6226dc
LYS <yesung5337@naver.com>
2022-04-07 17:05:33 +0900

    title play funtion

--- Discord Botfile/bot3.py
+++ Discord Botfile/bot3.py
@@ -17,7 +17,7 @@ async def on_ready():
     print('다음으로 로그인합니다: ')
     print(bot.user.name)
     print('connection was succeseful')
-    game = discord.Game('밥')
+    game = discord.Game('game')
     await bot.change_presence(status=discord.Status.online, activity = game)
 
 @bot.command()
@@ -29,7 +29,7 @@ async def 들어와(ctx):
         try:
             await vc.move_to(ctx.message.author.voice.channel)
         except:
-            await ctx.send('채널에 유저가 접속해있지 않네요..')
+            await ctx.send('채널어 유저가 접속해있지 않네요..')
 
 @bot.command()
 async def 나가(ctx):
@@ -52,6 +52,30 @@ async def URL재생(ctx, *, url):
     else:
         await ctx.send("노래가 이미 재생되고 있습니다!")
 
-bot.run(TOKEN)
+@bot.command()
+async def 재생(ctx, *, msg):
+    if not vc.is_playing():
+        global entireText
+        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            
+        chromedriver_dir = (r"C:\Users\c\Desktop\chromedriver.exe")
+        driver = webdriver.Chrome(chromedriver_dir)
+        driver.get("https://www.youtube.com/results?search_query="+msg+"+lyrics")
+        source = driver.page_source
+        bs = bs4.BeautifulSoup(source, 'lxml')
+        entire = bs.find_all('a', {'id': 'video-title'})
+        entireNum = entire[0]
+        entireText = entireNum.text.strip()
+        musicurl = entireNum.get('href')
+        url = 'https://www.youtube.com'+musicurl 
 
-#test
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+        vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+    else:
+        await ctx.send("이미 노래가 재생 중이라 노래를 재생할 수 없어요!")
+
+bot.run(TOKEN)
