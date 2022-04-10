6d015d0aa59c06135c14f631699eecc83097e40c
LYS <yesung5337@naver.com>
2022-04-10 19:20:24 +0900

    discord.py Changes due to end of development

--- Discord Botfile/bot3.py
+++ Discord Botfile/bot3.py
@@ -1,16 +1,16 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 from youtube_dl import YoutubeDL
 import bs4
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from discord.utils import get
-from discord import FFmpegPCMAudio
+from nextcord.utils import get
+from nextcord import FFmpegPCMAudio
 import asyncio
 import time
 import pandas as pd
 
-TOKENVALUE = open(r'C:\Users\c\Desktop\ndiscordTOKEN.txt','r')
+TOKENVALUE = open(r'C:\Users\c\Desktop\discordTOKEN.txt','r')
 TOKEN = TOKENVALUE.read()
 TOKENVALUE.close()
 
@@ -21,8 +21,8 @@ async def on_ready():
     print('다음으로 로그인합니다: ')
     print(bot.user.name)
     print('connection was succeseful')
-    game = discord.Game('음악 연구')
-    await bot.change_presence(status=discord.Status.online, activity = game)
+    game = nextcord.Game('음악 연구')
+    await bot.change_presence(status=nextcord.Status.online, activity = game)
 
 @bot.command()
 async def 들어와(ctx):
@@ -57,7 +57,7 @@ async def 링크재생(ctx, *, url):
             info = ydl.extract_info(url, download=False)
         URL = info['formats'][0]['url']
         vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
-        await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + url + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+        await ctx.send(embed = nextcord.Embed(title= "노래 재생", description = "현재 " + url + "을(를) 재생하고 있습니다.", color = 0x00ff00))
     else:
         await ctx.send("노래가 이미 재생되고 있습니다!")
 
@@ -88,7 +88,7 @@ async def 재생(ctx, *, msg):
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
         URL = info['formats'][0]['url']
-        await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+        await ctx.send(embed = nextcord.Embed(title= "노래 재생", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
         vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
     else:
         await ctx.send("이미 노래가 재생 중이라 노래를 재생할 수 없어요!")
@@ -98,7 +98,7 @@ async def 지금노래(ctx):
     if not vc.is_playing():
         await ctx.send("지금은 노래가 재생되지 않네요..")
     else:
-        await ctx.send(embed = discord.Embed(title = "지금노래", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+        await ctx.send(embed = nextcord.Embed(title = "지금노래", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
 
 @bot.command()
 async def 멜론차트(ctx):
@@ -127,7 +127,7 @@ async def 멜론차트(ctx):
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
         URL = info['formats'][0]['url']
-        await ctx.send(embed = discord.Embed(title= "노래 재생", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
+        await ctx.send(embed = nextcord.Embed(title= "노래 재생", description = "현재 " + entireText + "을(를) 재생하고 있습니다.", color = 0x00ff00))
         vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
     else:
         await ctx.send("이미 노래가 재생 중이라 노래를 재생할 수 없어요!")
